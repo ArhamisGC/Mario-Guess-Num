e7c470de7ca8370e6dedf0fb7f682779707d4820
hunte <hunterofseries13@gmail.com>
2024-08-12 12:29:46 +0100

    Implemented health display and dynamic update logic based on various in-game scenarios

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-
+const playBtn = document.getElementById("playBtn");
+const resetBtn = document.getElementById("restBtn");
 const guessingIpt = document.getElementById("guessing");
 const results = document.getElementById("results");
+const lifeId = document.getElementById("life");
+
+let lifeStr = lifeId.innerHTML;
+let life = parseInt(lifeStr);
+
 let randomNum = 0;
 let numberGuessValue = 0;
 let numberGuess = 0;
@@ -16,15 +22,35 @@ function playing(){
   numberGuess = parseInt(numberGuessValue);
   if(numberGuess === randomNum) {
     console.log("Winner");
-    results.innerHTML = "YOU WIN!"
+    results.innerHTML = "YOU WIN!";
   }else if(numberGuess < randomNum){
     results.innerHTML = "TRY A HIGHER NUMBER!";
+    lifeControl();
   }else{
     results.innerHTML = "TRY A LOWER NUMBER!";
+    lifeControl();
   }
 }
 
 function reset(){
   getRandomNumber();
   results.innerHTML = "WHAT IS YOUR GUESS?";
+  lifeControl('reset');
+}
+
+function lifeControl(action){
+  if(action === 'reset'){
+    life = 100;
+    lifeId.innerHTML = "100";
+    lifeId.style.backgroundImage = `linear-gradient(to right, red ${life}%, transparent ${life}%)`
+    playBtn.style.display = 'initial';
+  }else if('subtract'){
+    if(life === 20){
+      results.innerHTML = "GAME OVER";
+      playBtn.style.display = 'none';
+    }
+    life -= 20;
+    lifeId.innerHTML = life;
+    lifeId.style.backgroundImage = `linear-gradient(to right, red ${life}%,transparent ${life}%)`;
+  }
 }
